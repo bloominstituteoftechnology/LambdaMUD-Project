0df2b281ad1b4a0f7d224efa190712b996319d25
Lonnie Davis <ldavis0866@gmail.com>
2018-09-24 13:36:50 -0500

    whitenoise config

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
