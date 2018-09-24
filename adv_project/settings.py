2945c372667781fd7f1c1b168552734e8535a019
cmok4290 <cmok4290@gmail.com>
2018-09-24 18:21:59 -0400

    edited adv_project/settings.py

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -150,13 +150,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static',
-)
+# STATICFILES_DIR = (
+#     os.path.join(BASE_DIR, 'static',
+# )
 
 import django_heroku
 django_heroku.settings(locals())
