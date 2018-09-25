67526f4c73ae32ceae0675b47e0aa15cd8d01b4b
raymondgrc <raymond_grc@yahoo.com>
2018-09-25 16:46:42 -0500

    filler game screen

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Route, Link } from "react-router-dom";
 
 import SignIn from './components/sign-in/signin'
 import Register from './components/register/register'
+import Game from './components/game/game'
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Route exact path="/" component={SignIn} />
         <Route path="/register" component={Register} />
+        <Route path="/game" component={Game} />
       </div>
     );
   }
---
+++ ui/src/components/game/game.js
@@ -0,0 +1,49 @@
+import Paper from '@material-ui/core/Paper';
+import React from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Input from '@material-ui/core/Input';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    display: 'block', // Fix IE11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: '70%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  }
+})
+
+class Game extends React.Component {
+  render(){
+    return(
+      <main className={this.props.classes.layout}>
+        <Paper className={this.props.classes.paper}>
+          <Input />
+        </Paper>
+      </main>
+    )
+  }
+}
+// function Game(props){
+//   const { classes } = props;
+//   return(
+//   <React.Fragment>
+//     <CssBaseline />
+//     <Paper className={classes.paper}>
+//       Hello World
+//     </Paper>
+//   </React.Fragment>)
+// }
+
+export default withStyles(styles)(Game);
