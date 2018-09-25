bb64ce033963a4f46c5bd0a113093d8f317cb46c
Kevin Tena <kevten09@gmail.com>
2018-09-25 14:37:31 -0500

    Server is running with endpoints accessible from Postman

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -16,6 +16,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -147,7 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+
 
 import django_heroku
 django_heroku.settings(locals())
