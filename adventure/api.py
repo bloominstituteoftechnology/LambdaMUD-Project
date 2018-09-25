46e9dd5a3886ac44ba7d81ab8d93c229cc72c234
DirupT <iamcooled@gmail.com>
2018-09-25 18:13:49 -0500

    update return response of say method

--- adventure/api.py
+++ adventure/api.py
@@ -72,8 +72,9 @@ def say(request):
     data = json.loads(request.body)
     message = data['message']
     room = player.room()
+    players = room.playerNames(player_id)
     currentplayerUUIDs = room.playerUUIDs(player_id)
     for p_uuid in currentplayerUUIDs:
         pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {
-                       'message': f'{player.user.username}: {message}'})
-    return JsonResponse({'name': player.user.username, 'message': message, 'error_msg': ""}, safe=True)
+                       'message': f'{message}'})
+    return JsonResponse({'name': player.user.username, 'title': room.title, 'description': room.description, 'players': players, 'message': message, 'error_msg': ""}, safe=True)
