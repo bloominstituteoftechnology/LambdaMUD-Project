993bce156bde6b8b3da4cad4f562b765165057d7
Simon <Simon_Freelance@hotmail.com>
2018-09-24 20:36:44 -0400

    Removed duplicate BASE_DIR

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -146,9 +146,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
