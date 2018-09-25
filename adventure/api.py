b38b1a6bc29439f5d4447b2e74764e81e5e69356
Nicky Chen <nicky.chen0@gmail.com>
2018-09-25 13:49:55 -0700

    pusher change

--- adventure/api.py
+++ adventure/api.py
@@ -9,7 +9,8 @@ from rest_framework.decorators import api_view
 import json
 
 # instantiate pusher
-# pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
+pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config(
+    'PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
 
 
 @csrf_exempt
