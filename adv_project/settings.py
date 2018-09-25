c81561dc4d9e72a741a960bd68eec7d6ead26d58
Katie Gorbell <katiegorbell@gmail.com>
2018-09-25 17:13:22 -0500

    settings updated - typo

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pusherable',
+    'pusher',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
