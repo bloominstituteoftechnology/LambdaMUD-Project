bdbfc517d8e2c7b18e9f7ab1f93bb534849c8660
Austin Merando <ajmerando1@yahoo.com>
2018-09-25 14:40:05 -0700

    bug fix attempt

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -64,10 +64,10 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
