afc2c9f29a18ce3185d9b208b73a5bbf18df6b67
Simon <Simon_Freelance@hotmail.com>
2018-09-24 20:13:26 -0400

    re-added static options

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -148,9 +148,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 import django_heroku
 django_heroku.settings(locals())
 
