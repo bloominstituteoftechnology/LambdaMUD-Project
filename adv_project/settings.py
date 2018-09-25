639ce281cb5d1e6b2588b87b111c0e43515adb0e
Simon <Simon_Freelance@hotmail.com>
2018-09-24 20:45:11 -0400

    STATIC_ROOT Re-added

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -150,6 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 
