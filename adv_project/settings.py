432eb3fb17152fd7d1699ffb409bcf27e782a65e
Simon <Simon_Freelance@hotmail.com>
2018-09-25 12:22:40 -0400

    Deployment try fix

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -149,8 +149,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIRS = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Extra places for collectstatic to find static files.
 
