219b2109c523205c33fe85518c4a50716d3a8a63
sibhat <sibhat.temelso@gmail.com>
2018-09-25 12:28:31 -0700

    wsgi whitenoise added

--- adv_project/wsgi.py
+++ adv_project/wsgi.py
@@ -16,3 +16,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adv_project.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
