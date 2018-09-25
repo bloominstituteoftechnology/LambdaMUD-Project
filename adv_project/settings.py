81eee8608aec547643000b6ed50cc9ea45e20642
CLN <cln@Sags-MacBook-Pro.local>
2018-09-25 12:10:15 -0400

    ,.

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -152,6 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './adv_project/static/'
 
 import django_heroku
 django_heroku.settings(locals())
