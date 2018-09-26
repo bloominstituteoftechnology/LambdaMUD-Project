735caaccb7e1efeb00ea25f7e907704f11ab372b
Lonnie Davis <ldavis0866@gmail.com>
2018-09-26 11:39:48 -0500

    change varible in DATABASE confi

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(default=config('HEROKU'))
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
