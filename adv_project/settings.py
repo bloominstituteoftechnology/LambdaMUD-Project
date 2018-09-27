7037f758d210deeef3c83f04e29fd4e9344591c0
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-27 00:54:57 -0700

    hardcoding hosts for cors troubleshooting

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+# ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://noxasaxon.github.io/LambdaMUD-Client/']
 
 
 # Application definition
