e9391613eff277fc60caa93a7324dccf0348ac53
Gabriel Duquette <gabrielduquette@gmail.com>
2018-09-26 23:46:17 -0400

    add say

--- adventure/api.py
+++ adventure/api.py
@@ -63,5 +63,16 @@ def move(request):
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
-    # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    player = request.user.player
+    player_id = player.id
+    data = json.loads(request.body)
+    message = data['message']
+    room = player.room()
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+
+    if message:
+	    for p_uuid in currentPlayerUUIDs:
+	        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username}: 'f'{message}'})
+	    return JsonResponse({'name':player.user.username, 'message':message, 'error_msg':""}, safe=True)
+    else:
+	    return JsonResponse({'error':'Message could not be sent'}, safe=True, status=500)
