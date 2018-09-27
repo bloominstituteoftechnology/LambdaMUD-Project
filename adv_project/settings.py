d0c1b3a6316e1d5d7185b4900596d47541771b64
noxasaxon <34722463+noxasaxon@users.noreply.github.com>
2018-09-27 02:04:45 -0700

    fixing test

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -153,9 +153,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
