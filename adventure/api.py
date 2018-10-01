ccc471cfaa0dffef36770ab12bc35995be5edcb0
JJ Ashcraft <jj@jjashcraft.com>
2018-10-01 16:18:20 -0400

    bug fix

--- adventure/api.py
+++ adventure/api.py
@@ -75,5 +75,5 @@ def say(request):
     room = player.room()
     currentPlayerUUIDs = room.playerUUIDs(player_id)
     for p_uuid in currentPlayerUUIDs:
-            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username}: {data.message}'})
+            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username}: {data}'})
     return JsonResponse({'Success':"Your message was sent."}, safe=True, status=200)
