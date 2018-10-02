623fa9ff886ac7a49088a4bce6c39edff9061213
Daniel Chamorro <dan@dcitllc.com>
2018-10-02 18:49:12 -0400

    Trying to fix * Exception Value: 'Player' object has no attribute 'urls'

--- adventure/admin.py
+++ adventure/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from adventure.models import Room, Player
 
 # Register your models here.
-admin.site.register(Room, Player)
+admin.site.register(Room)
+admin.site.register(Player)
