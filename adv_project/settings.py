bbe2f61a0cb2fba095718a26529ad2f13505f8aa
Adrian Adames <adrianadames@gmail.com>
2018-10-03 04:33:18 -0400

    Troubleshooting server error (500) message.

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -74,7 +74,8 @@ ROOT_URLCONF = 'adv_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
