3c5e16445432848f761b8a44d535323abff45d61
Adrian Adames <adrianadames@gmail.com>
2018-10-03 05:10:23 -0400

    Added permissions so that anonymous user has read-only access to mud game.

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -130,6 +130,7 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
