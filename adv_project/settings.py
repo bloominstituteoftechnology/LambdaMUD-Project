2448fc1132f45003d17987f1762dd293679fa33c
Adrian Adames <adrianadames@gmail.com>
2018-10-03 04:37:21 -0400

    Undid change to templates in settings.py.

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -74,8 +74,8 @@ ROOT_URLCONF = 'adv_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        # 'DIRS': [],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
