b54c88095dabf68a3af5b70d8cc6900c843810c3
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-03 07:25:23 -0700

    test

--- adventure/api.py
+++ adventure/api.py
@@ -64,4 +64,13 @@ def move(request):
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    message = data['message']
+    room = player.room()
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDs:
+            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says {message}.'})
+    return JsonResponse({'username':player.user.username, 'message': message}, safe=True, status=200)
