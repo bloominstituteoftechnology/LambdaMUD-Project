9fe6d2909dbd4bc7ec2f21312d9281851afc3705
Daniel Chamorro <dan@dcitllc.com>
2018-10-05 00:28:23 -0400

    Added Views for index and Registered

---
+++ adventure/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from adventure.forms import UserForm, UserProfileInfoForm
+
+#Views
+
+def index(request):
+    return render(request, 'adventure/index.html')
+
+def register(request):
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        profile_form = UserProfileInfoForm(data=request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pic']
+
+            profile.save()
+            registered = True
+
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'adventure/registration.html', 
+    {
+       'user_form': user_form,
+       'profile_form': profile_form,
+       'registered': registered 
+    })
