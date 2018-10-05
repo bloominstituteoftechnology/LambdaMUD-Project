8cd91eb392514bc2904c823d1b6bd0b9c8aeb9bc
Daniel Chamorro <dan@dcitllc.com>
2018-10-05 11:35:47 -0400

    Renamed Model Field in UserProfileInfo

--- adventure/models.py
+++ adventure/models.py
@@ -64,7 +64,7 @@ def save_user_player(sender, instance, **kwargs):
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    portfolio = models.URLField(blank=True)
+    portfolio_site = models.URLField(blank=True)
     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
