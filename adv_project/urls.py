70a739dde1c465c726d50c3056f387dee67ceb00
Daniel Chamorro <dan@dcitllc.com>
2018-10-05 00:11:16 -0400

    added addtional urls

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
+from adventure import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    path('', views.index, name='index'),
+    path('adventure/', include('adventure.urls')),
 ]
--- adventure/urls.py
+++ adventure/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 from . import api
+from adventure import views
+
+app_name = 'adventure'
 
 urlpatterns = [
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
+    url(r"^register/$", views.register, name="register"),
+    
 ]
