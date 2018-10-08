e565bc7edbbae80608c5df2cfd3ec540224a6b3a
Mark Hong <m4rkh0ng@gmail.com>
2018-10-08 13:05:19 -0400

    fix player_id in adventure/api.py

--- adventure/api.py
+++ adventure/api.py
@@ -70,7 +70,7 @@ def move(request):
             pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
     else:
-        players = room.playerNames(player_uuid)
+        players = room.playerNames(player_id)
         return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
