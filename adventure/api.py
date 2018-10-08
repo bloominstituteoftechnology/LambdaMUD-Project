a3c52c627ca386d9d28529a320db66cd3eb6550f
unknown <may.alex43@gmail.com>
2018-10-08 16:21:32 -0400

    implemented say command

--- adventure/api.py
+++ adventure/api.py
@@ -64,4 +64,13 @@ def move(request):
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    message = " ".join(data["message"].split(" ")[1:])
+    room = player.room()
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDs:
+        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says "{message}"'})
     return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
