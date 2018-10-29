e9c01c84bda8fd6f58ad9e5ca9b62c3a19e27405
adamcglee <adamcglee@gmail.com>
2018-10-29 16:20:00 -0700

    updated issue with say function

--- adventure/api.py
+++ adventure/api.py
@@ -74,5 +74,5 @@ def say(request):
         for roomplayer in playerUUIDs:
             pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username}: {message}'})
             return JsonResponse({'name':player.user.username, 'message':message}, safe=True)
-        else:
-            return JsonResponse({'error':"Something went wrong."}, safe=True, status=500)
+    else:
+        return JsonResponse({'error':"Something went wrong."}, safe=True, status=500)
