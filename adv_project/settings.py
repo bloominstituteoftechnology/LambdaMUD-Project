c2e98effa6b41375adcefc6a264941fe12f250dd
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-29 19:52:08 -0400

    DEBUG

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -106,8 +106,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
 
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
