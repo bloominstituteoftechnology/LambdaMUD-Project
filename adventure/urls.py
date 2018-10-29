676a9be44e6dd70bd617f5a0d6f008addb5021f3
francistse23 <wtse.ucsd@gmail.com>
2018-10-29 14:29:05 -0700

    commented out some lines in urls.py.

--- adventure/urls.py
+++ adventure/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from . import api
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    # url(r'^login/$', auth_views.login, name='login'),
+    # url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
