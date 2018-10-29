b055b8a3804e02d2e25bdbcf2ac410a502b180a8
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-10-29 09:41:52 -0700

    hardcode app_id

--- adventure/api.py
+++ adventure/api.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view
 import json
 
 # instantiate pusher
-pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
+pusher = Pusher(app_id='634432', key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
 
 @csrf_exempt
 @api_view(["GET"])
