8cbc2e23c45e3816aed5e33a7cdf77f48f8517c0
JoCatherine Griggs <jocafrin@gmail.com>
2018-10-29 19:12:06 -0400

    pre-deployment commit

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -14,6 +14,14 @@ import os
 from decouple import config
 import pusher
 
+pusher_client = pusher.Pusher(
+  app_id= config('PUSHER_APP_ID'),
+  key= config('PUSHER_KEY'),
+  secret= config('PUSHER_SECRET'),
+  cluster= config('PUSHER_CLUSTER'),
+  ssl=True
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
