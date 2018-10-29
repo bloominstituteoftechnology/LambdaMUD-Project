9cffda25fb8ab997b44ac5f9c2f37f266a295d4b
mkerbleski <mkerbles@gmail.com>
2018-10-29 13:15:11 -0600

    add pusher credentials to .env

--- api/apps.py
+++ api/apps.py
@@ -3,3 +3,17 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+
+# i don't know if this is where it goes
+import pusher
+
+pusher_client = pusher.Pusher(
+  app_id='634597',
+  key='4830aec0ca635aa67084',
+  secret='f6e93311137ccd4173c9',
+  cluster='us2',
+  ssl=True
+)
+
+pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
