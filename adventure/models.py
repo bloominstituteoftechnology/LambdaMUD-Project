a9b6b0fef822f4b34ba953ae4b221a06c225f16f
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-10-30 11:00:38 -0700

    comment on necessary source files

--- adventure/models.py
+++ adventure/models.py
@@ -5,6 +5,8 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 import uuid
 
+# creates models for the database
+
 class Room(models.Model):
     title = models.CharField(max_length=50, default="DEFAULT TITLE")
     description = models.CharField(max_length=500, default="DEFAULT DESCRIPTION")
--- adventure/urls.py
+++ adventure/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import api
 
+# allows for different url endpoints
+
 urlpatterns = [
     url('init', api.initialize),
     url('move', api.move),
