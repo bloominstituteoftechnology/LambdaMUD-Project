63c1ce8f95f571e93baf6fa202ac57fd952ef02d
keegn <keegan.t.burkett@gmail.com>
2018-10-30 18:21:40 -0500

    Updated settings as test

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -104,8 +104,17 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # }
 
 # Heroku setup
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
 
 
 
@@ -159,9 +168,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# # Heroku setup
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-# Heroku setup
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import django_heroku
 django_heroku.settings(locals())
