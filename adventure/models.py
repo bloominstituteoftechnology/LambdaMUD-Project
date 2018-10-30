78be513a79a60221bc6f345cd3c8b102817d1c86
MichaelBoegner <cmikeb@gmail.com>
2018-10-30 19:03:16 -0400

    Altered model to allow for current player in playernames nad playeruuid methods

--- adventure/models.py
+++ adventure/models.py
@@ -31,10 +31,10 @@ class Room(models.Model):
                 print("Invalid direction")
                 return
             self.save()
-    def playerNames(self, currentPlayerID):
-        return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
-    def playerUUIDs(self, currentPlayerID):
-        return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+    def playerNames(self):
+        return [p.user.username for p in Player.objects.filter(currentRoom=self.id)]
+    def playerUUIDs(self):
+        return [p.uuid for p in Player.objects.filter(currentRoom=self.id)]
 
 
 class Player(models.Model):
