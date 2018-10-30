16a730136d90084faae7f9941954dac0c8eb1681
keegn <keegan.t.burkett@gmail.com>
2018-10-30 18:33:35 -0500

    More settings updates

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -104,16 +104,10 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # }
 
 # Heroku setup
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 
 
@@ -171,8 +165,9 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # # Heroku setup
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 import django_heroku
 django_heroku.settings(locals())
