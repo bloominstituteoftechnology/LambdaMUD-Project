ea8f00a59655dd003bf2b87c97218888b3ae7ff4
Will Kwon <willkwon@Williams-MacBook-Air.local>
2018-10-30 00:00:01 -0700

    added a needed comma

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -55,7 +55,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
