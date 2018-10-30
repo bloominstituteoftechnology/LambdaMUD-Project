16f1377d30432bfd582a608f846338057cbfec50
kaibo <38451683+noshaikh@users.noreply.github.com>
2018-10-30 03:43:26 -0700

    sslmode debug

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -159,3 +159,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import django_heroku
 django_heroku.settings(locals())
+
+
+del DATABASES['default']['OPTIONS']['sslmode'] # <-- Add this line
