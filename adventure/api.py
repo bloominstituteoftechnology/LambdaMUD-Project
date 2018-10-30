7bead231b5cc0554e3c34a8f600c81e82251f48a
Will Kwon <willkwon@Williams-MacBook-Air.local>
2018-10-29 19:18:20 -0700

    wrote say function

--- adventure/api.py
+++ adventure/api.py
@@ -63,5 +63,17 @@ def move(request):
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
-    # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    # IMPLEMENT 
+    data=json.loads(request.body)
+    message=data['message']
+    user=request.user
+    player=user.player
+    player_id=player.id
+    uuid=player.uuid
+    room=player.room()
+    players=room.playerNames(player_id)
+    currentPlayerUUIDS = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDS:
+        pusher.trigger(f'p-channel-{p_uuid}',u'broadcast',{'message':f'{player.user.username} says {message}'})
+    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
+    
