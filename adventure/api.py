73a8ea2b2f2b927f5c8e1ccd893b89e28bd73dd7
meltzerkyle <meltzerkyle@gmail.com>
2018-10-30 01:24:27 -0500

    minor fix to hello

--- adventure/api.py
+++ adventure/api.py
@@ -70,5 +70,5 @@ def say(request):
     room = player.room()
     currentPlayerUUIDs = room.playerUUIDs(player_id)
     for p_uuid in currentPlayerUUIDs:
-        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says, "Hello, world!"'})
+        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says, \'Hello, world!\''})
     return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'error_msg':""}, safe=True, status=500)
