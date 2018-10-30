5fb65a0a0e3aa41aa9976562a9d2ba90cfffecee
kaibo <38451683+noshaikh@users.noreply.github.com>
2018-10-30 02:42:48 -0700

    fixed typ

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -56,7 +56,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
