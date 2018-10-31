7eb9fb595cf40ba4d45bcfb4223d075462824f6d
Grant Reighard <grant.reighard@gmail.com>
2018-10-31 18:23:56 -0400

    Fixed dir().

--- adventure/api.py
+++ adventure/api.py
@@ -98,7 +98,7 @@ def shout(request):
 @csrf_exempt
 @api_view(["POST"])
 def whisper(request):
-    print(dir(request.data.keys, request.data.keys()))
+    print(dir(request.data.keys)
     player = request.user.player
     player_id = player.id
     toUser = request.data.toUser
