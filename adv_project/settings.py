6920894f7b812098daa98c6f07ef099bb0651315
keegn <keegan.t.burkett@gmail.com>
2018-10-30 19:41:38 -0500

    Settings ...

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -104,11 +104,17 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # }
 
 # Heroku setup
-DATABASES = {}
+# DATABASES = {}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-# DATABASES["default"] = dj_database_url.config(default=config('DATABASE_URL'),conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
 
