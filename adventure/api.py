83dec97aa07bfca05b59ee0a17c37e98d4bdd1f2
Grant Reighard <grant.reighard@gmail.com>
2018-10-31 17:08:44 -0400

    Trying to access UUID property.

--- adventure/api.py
+++ adventure/api.py
@@ -89,7 +89,7 @@ def shout(request):
     for room in allRooms:
         allUUIDs.append(room.playerUUIDs(player_id))
     for p_uuid in allUUIDs:
-        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} shouts {request.data["message"]}!'})
+        pusher.trigger(f'p-channel-{p_uuid[0][0]}', u'broadcast', {'message':f'{player.user.username} shouts {request.data["message"]}!'})
     room = player.room()
     players = room.playerNames(player_id)
     return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'message':request.data['message']}, safe=True)
