8c3ba43449d04afb05b7b282a91966d01b2382f1
MichaelBoegner <cmikeb@gmail.com>
2018-11-01 14:50:21 -0400

    changing plot

--- adventure/create_world.py
+++ adventure/create_world.py
@@ -4,11 +4,11 @@ from adventure.models import Player, Room
 
 Room.objects.all().delete()
 
-r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
+r_jappartment = Room(title="Jerry's Appartment",
+               description="You find yourself on Jerry's couch with no memory of how you got there. You look around, and it's, uhhh, it's an apartment. You hear Jerry snoring in the other room. Possible Moves: (s to leave apartment, n to go into Jerry's room, w to go to the kitchen) ")
 
-r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-passages run north and east.""")
+r_jroom = Room(title="Jerry's Room", 
+description="""You walk groggly into Jerry's room and stand over him, two inches from his face to see if he's awake. You lose your balance and knock over everything on his bedside table, waking him up. He shouts, "What are you doing Larry!? Get out!!" Possible Moves: (s back to the living room)""")
 
 r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -21,15 +21,15 @@ r_treasure = Room(title="Treasure Chamber", description="""You've found the long
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""")
 
-r_outside.save()
-r_foyer.save()
+r_jappartment.save()
+r_jroom.save()
 r_overlook.save()
 r_narrow.save()
 r_treasure.save()
 
 # Link rooms together
-r_outside.connectRooms(r_foyer, "n")
-r_foyer.connectRooms(r_outside, "s")
+r_jappartment.connectRooms(r_jroom, "n")
+r_jroom.connectRooms(r_jappartment, "s")
 
 r_foyer.connectRooms(r_overlook, "n")
 r_overlook.connectRooms(r_foyer, "s")
