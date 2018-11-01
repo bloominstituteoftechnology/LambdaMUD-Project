b344be90ef3acf777122763ae899497d08203e66
keegn <keegan.t.burkett@gmail.com>
2018-11-01 15:33:21 -0500

    Updated settings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -108,12 +108,14 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
 
 
