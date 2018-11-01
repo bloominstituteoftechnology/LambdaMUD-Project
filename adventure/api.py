bbae41142b3ed1d93acabb72fe969d4b7114f1d7
LizBaker <lizbaker.pdx@gmail.com>
2018-11-01 14:51:46 -0700

    Adding error to move func

--- adventure/api.py
+++ adventure/api.py
@@ -57,7 +57,7 @@ def move(request):
         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_uuid)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({"error":"You cannot move that way"}, safe=True, status=500)
 
 
 @csrf_exempt
