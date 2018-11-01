9f6ba11d3778bcb9485b9d4bb1fd61764b1d60ef
“Kennedykid1995” <34872885+Kennedykid1995@users.noreply.github.com>
2018-10-31 21:08:44 -0500

    pushing

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'adv_project.urls'
