67025a53eca8ebb97aef91eeb0f0519240619a3a
LaikaFusion <newrad4@gmail.com>
2018-11-01 10:55:25 -0400

    changed field

--- adventure/migrations/0001_initial.py
+++ adventure/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(default='DEFAULT TITLE', max_length=50)),
-                ('description', models.CharField(default='DEFAULT DESCRIPTION', max_length=500)),
+                ('description', models.CharField(default='DEFAULT DESCRIPTION', max_length=5000)),
                 ('n_to', models.IntegerField(default=0)),
                 ('s_to', models.IntegerField(default=0)),
                 ('e_to', models.IntegerField(default=0)),
