f06b6ba388713bf2194bafe3463ca29748f736bc
Adrian Adames <adrianadames@gmail.com>
2018-11-24 14:27:07 -0500

    Deleted artifact directory that I had created when I initially started working on the LambdaMUD project and was planning to work on the server and client code in the same directory. For reasons I don't want to spend time having to recall, I created a seperate github repo for the client part of the LambdaMUD project.

--- server/adventure/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdventureConfig(AppConfig):
-    name = 'adventure'
--- server/adventure/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import api
-
-urlpatterns = [
-    url('init', api.initialize),
-    url('move', api.move),
-    url('say', api.say),
-]
