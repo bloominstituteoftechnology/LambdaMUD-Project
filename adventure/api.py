d8648293e25095946cfd1923b60a45cb1d9c1e2b
unknown <jesse.reichel@live.com>
2018-11-27 19:50:41 -0800

    implemented say?

--- adventure/api.py
+++ adventure/api.py
@@ -63,5 +63,10 @@ def move(request):
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
-    # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    pusher.trigger(f'p-channel-{player_uuid}', u'broadcast', {'message':f'{player.user.username} says {data}.'})
+
+    return JsonResponse({'name':player.user.username, 'message':data}, safe=True)
