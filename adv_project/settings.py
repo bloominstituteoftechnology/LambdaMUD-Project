919f962524ef7b36eb3444b198735fd959166863
unknown <jesse.reichel@live.com>
2018-12-03 21:40:02 -0800

    db?

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -56,7 +56,7 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
