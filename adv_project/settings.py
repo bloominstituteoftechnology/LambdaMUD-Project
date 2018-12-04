9c4f5f7f4bfc864cb9a3c51f703666ab882fdb47
JohnCoronel <john.m.coronel@gmail.com>
2018-12-03 20:23:09 -0800

    backend server

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -55,6 +55,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
