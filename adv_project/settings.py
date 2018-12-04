b6bd00e3c34a497644ccc7d466fada2c101ffc33
unknown <jesse.reichel@live.com>
2018-12-03 18:14:07 -0800

    re-deploying

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -89,12 +89,9 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+
 
 
 # Password validation
@@ -148,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 import django_heroku
 django_heroku.settings(locals())
