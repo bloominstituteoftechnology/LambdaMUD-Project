035a4fe41adfe77ca5f8e1388f52112afe02a9cb
iamthuypham <tpham045@hotmail.com>
2018-12-04 20:13:13 -0800

    Say to current room only

--- adventure/api.py
+++ adventure/api.py
@@ -51,11 +51,11 @@ def move(request):
         players = nextRoom.playerNames(player_id)
         currentPlayerUUIDs = room.playerUUIDs(player_id)
         nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
-        for p_uuid in currentPlayerUUIDs:
-            pusher.trigger(f'global', u'broadcast', {'id': f'{uuid.uuid4().hex}','message': f'{player.user.username} has walked {dirs[direction]}.'})
+
         for p_uuid in nextPlayerUUIDs:
-            pusher.trigger(f'global', u'broadcast', {'id': f'{uuid.uuid4().hex}','message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
+            pusher.trigger(f'{player.p_uuid}', f'{nextRoomID}', {'id': f'{nextRoomID}','message':f'{player.user.username} just entered {nextRoom.title}.', 'playerName': f'{player.user.username}')
         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
+
     else:
         players = room.playerNames(player_uuid)
         return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
