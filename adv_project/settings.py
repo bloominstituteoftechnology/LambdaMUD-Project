a09f61050b5ba248f6d538fa3a59704a9e61d15e
JohnCoronel <john.m.coronel@gmail.com>
2018-12-05 22:14:34 -0800

    test

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -149,7 +149,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import django_heroku
 django_heroku.settings(locals())
