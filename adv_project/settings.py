b4b18ccedff3ee5a04c7905f8493a716bce0e0f3
JohnCoronel <john.m.coronel@gmail.com>
2018-12-05 23:14:27 -0800

    heroku

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'adv_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
