692ff169062ad8b46da41dd69c97667aed5e7998
JohnCoronel <john.m.coronel@gmail.com>
2018-12-05 23:43:08 -0800

    db

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 
 DATABASES = {}
     
-DATABSES['default'] = dj_database_url.config(default=config('DATABASE_URL'),conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'),conn_max_age=600)
     
 
 
