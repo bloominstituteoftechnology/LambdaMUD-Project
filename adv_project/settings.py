05ae35eae2ee6bb2a7b735c2d2f596ad2fc2e4b5
JohnCoronel <john.m.coronel@gmail.com>
2018-12-05 22:21:55 -0800

    staticfile update

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -152,3 +152,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import django_heroku
 django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
