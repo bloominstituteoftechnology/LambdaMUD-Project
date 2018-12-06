0359f6175b926c0e9572d1f98ae6cd9da432066d
iamthuypham <tpham045@hotmail.com>
2018-12-05 18:16:09 -0800

    Broadcast message only people in room but not current user

--- adventure/api.py
+++ adventure/api.py
@@ -53,7 +53,8 @@ def move(request):
         nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
 
         for p_uuid in nextPlayerUUIDs:
-            pusher.trigger(f'{nextRoomID}', f'message', {'id': f'{uuid.uuid4().hex}','message':f'{player.user.username} just entered {nextRoom.title}.'})
+            if player.uuid is not p_uuid:
+                pusher.trigger(f'{nextRoomID}', f'message', {'id': f'{uuid.uuid4().hex}','message':f'{player.user.username} just entered {nextRoom.title}.'})
         return JsonResponse({'currentRoomId': nextRoomID,'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
 
     else:
