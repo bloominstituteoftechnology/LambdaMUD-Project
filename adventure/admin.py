02544147585e507338d615833c0efcc9cbfb3614
Csillag61 <csillagok@gmail.com>
2018-12-09 13:28:03 -0500

    adv.model registered

--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from adventure.models import Room, Player
+
+
+class AdventureAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Room, AdventureAdmin)
+admin.site.register(Player, AdventureAdmin)
