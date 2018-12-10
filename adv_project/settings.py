07df28dc65a2c7a2ba5daf1072bc375ea96814eb
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-12-10 13:06:46 -0800

    fixed database

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 
