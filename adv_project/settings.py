bf3d0f436cc11dabac591946788d9eb661f7298f
Luisan <luisangelsc@gmail.com>
2018-12-10 19:47:32 -0600

    deployed to heroku

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -29,6 +29,8 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+# PUSHER_APP_ID = config('PUSHER_APP_ID', cast=int)
+
 
 # Application definition
 
