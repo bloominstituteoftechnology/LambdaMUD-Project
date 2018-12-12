599af89a46cbc71d37e1bd7114f190294204bc66
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-12-11 16:42:53 -0800

    added line to settings.py to remove session authentication

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
