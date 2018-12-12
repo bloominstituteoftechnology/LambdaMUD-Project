48af05ac5e93dbc4c48bf119793ad53306624bd5
Andrew Liao <andrewoliao@gmail.com>
2018-12-11 16:11:23 -0800

    Add to REST_FrameWORK permissions

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -123,6 +123,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+       'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
 }
 
 CORS_ORIGIN_ALLOW_ALL=True
