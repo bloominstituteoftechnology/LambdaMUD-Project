462e90b4e99733ba92cd2be0691797838e140103
Adrian Adames <adrianadames@gmail.com>
2018-12-12 16:44:03 -0500

    Troubleshoting 'say' endpoint cont...

--- adventure/api.py
+++ adventure/api.py
@@ -86,7 +86,7 @@ def say(request):
     player = request.user.player
     # player_id = player.id
     player_uuid = player.uuid
-    # data = json.loads(request.body['say'])
+    data = json.loads(request.body['say'])
     # pusher.trigger(f'p-channel-{player_uuid}', u'say', {'message':f'{player.user.username} says {data}.'})
     pusher.trigger(f'p-channel-{player_uuid}', u'say', {'message':f'{player.user.username} says ahoy back to server.'})
     # return JsonResponse({'say': data}, safe=True, status=200)
