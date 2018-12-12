8ad598611dd939185c1eb0e23c96bdb06902f87d
Adrian Adames <adrianadames@gmail.com>
2018-12-12 01:03:40 -0500

    Commented out local host database (sqlite3) and uncommented heroku database.

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -94,17 +94,17 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# # FOR RUNNING DATABASE ON LOCAL SERVER
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# # # FOR RUNNING DATABASE ON LOCAL SERVER
 # DATABASES = {
-#     'default': dj_database_url.config(default = config('DATABASE_URL')),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(default = config('DATABASE_URL')),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
