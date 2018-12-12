e820179855402b933ab7fd41dc988b86513c551a
Lucas Beemer <lucasbeemer@gmail.com>
2018-12-11 22:23:45 -0800

    new database

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -102,9 +102,6 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# DATABASES = {}
-# DATABASES['default'] = dj_database_url.config(default=os.getenv('DATABASE_URL'))
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
