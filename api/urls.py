796fd44a9c01369ad4f466e12a532469deb727d4
Csillag61 <csillagok@gmail.com>
2018-12-12 15:03:45 -0500

    login and register

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,8 @@ from django.urls import include, path
 from django.conf.urls import url
 
 urlpatterns = [
-    path('', include('rest_auth.urls')),
-    path('registration/', include('rest_auth.registration.urls')),
+    url('registration', views.register),
+    url('login', views.login),
+#     path('', include('rest_auth.urls')),
+#     path('registration/', include('rest_auth.registration.urls')),
 ]
