f4dfc195382adbb0de3c7b60d9f7432d210a6f90
tsaihuangsd <tsaihuangsd@gmail.com>
2018-12-11 18:37:04 -0800

    Clear DATABASES variable in settings.py

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
