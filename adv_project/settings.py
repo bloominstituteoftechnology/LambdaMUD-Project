363f37dacda03c3faf057f858814840197b0a227
Andrew Liao <andrewoliao@gmail.com>
2018-12-11 19:33:05 -0800

    Remove session authentication to address CRSF

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -120,11 +120,7 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ),
-       'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
     ),
 }
 
