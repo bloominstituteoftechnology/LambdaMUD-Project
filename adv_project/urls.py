f1b9517c00ed684ab30ab553e0df70204f884063
verydecent <wnjhwng@gmail.com>
2018-12-13 12:52:26 -0800

    update

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    path('api-auth', include('rest_framework.urls'))
 ]
