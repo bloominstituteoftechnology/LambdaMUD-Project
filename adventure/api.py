77925c750124ba32c0cf75c32da6dbc61f44474a
iAmAdamReid <adam@adamreid.me>
2018-12-12 20:35:13 -0800

    say parses request body message

--- adventure/api.py
+++ adventure/api.py
@@ -64,4 +64,19 @@ def move(request):
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    body = json.loads(request.body)
+    message = body['message']
+    print(message)
+    
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    room = player.room()
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDs:
+        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username}: {message}'})
+    # push event to pusher
+
+    # trigger response
+
+    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=200)
