e5f9052c05243e3732c89b46e9a9a5bce0023625
Akshay Gadkari <akshay.gadkari@gmail.com>
2018-12-12 23:27:45 -0800

    changed database config in settings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
