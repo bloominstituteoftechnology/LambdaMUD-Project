3d334d02c435b06f59a592d3bbcf5c62752f3e02
Lucas Beemer <lucasbeemer@gmail.com>
2018-12-13 03:24:32 -0800

    renamed db

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
     }
 }
 
