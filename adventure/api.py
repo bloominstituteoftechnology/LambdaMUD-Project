f3c8e6566cd839ac9fec850f1df74f6f1a7966cb
James Cassidy <cassidyjamesw@gmail.com>
2018-12-13 13:33:42 -0500

    another say attempt

--- adventure/api.py
+++ adventure/api.py
@@ -57,20 +57,18 @@ def move(request):
         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_id)
-        return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
+        return JsonResponse({'name': player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
-    # IMPLEMENT
     player = request.user.player
-    player_id = player.id
+    player_name = player.user.username
     player_uuid = player.uuid
+    player_id = player.id
     data = json.loads(request.body)
-    saidmessage = data['saidmessage']
-    room = player.room()
-    currentPlayerUUIDs = room.playerUUIDs(player_id)
-    for p_uuid in currentPlayerUUIDs:
-        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message': f'{player.user.username} says: {saidmessage}'})
-    return JsonResponse({'name': player.user.username, 'saidmessge': f'{player.user.username} says: {saidmessage}', 'usersinroom': currentPlayerUUIDs}, safe=True)
+    message = data['message']
+
+    pusher.trigger(f'p-channel-{player_uuid}', u'broadcast', {'name':f'{player_name}', 'message':f'{message}'})
+    
