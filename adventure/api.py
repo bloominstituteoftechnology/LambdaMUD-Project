b7f2218c6546b888cf2abd761e910e09a4979819
mparedes003 <michelle.g.paredes@gmail.com>
2018-12-13 10:42:43 -0800

    Added headers for api.py

--- adventure/api.py
+++ adventure/api.py
@@ -13,6 +13,7 @@ pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret
 
 @csrf_exempt
 @api_view(["GET"])
+# initializes a player in a room
 def initialize(request):
     user = request.user
     player = user.player
@@ -20,11 +21,13 @@ def initialize(request):
     uuid = player.uuid
     room = player.room()
     players = room.playerNames(player_id)
+    # returns uuid, player's username, the room they are in and the description of the room
     return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
 
 
 # @csrf_exempt
 @api_view(["POST"])
+# moves a player through the world in one direction at a time: n, e, w, s
 def move(request):
     dirs={"n": "north", "s": "south", "e": "east", "w": "west"}
     reverse_dirs = {"n": "south", "s": "north", "e": "west", "w": "east"}
@@ -51,12 +54,17 @@ def move(request):
         currentPlayerUUIDs = room.playerUUIDs(player_id)
         nextPlayerUUIDs = nextRoom.playerUUIDs(player_id)
         for p_uuid in currentPlayerUUIDs:
+            # broadcasts in which direction a player has walked out of a room
             pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has walked {dirs[direction]}.'})
         for p_uuid in nextPlayerUUIDs:
+            # broadcasts that a player has entered a room and from which direction they have entered from
             pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} has entered from the {reverse_dirs[direction]}.'})
+        # returns player's username, the name of the room the player has entered, its description and the players that are currently in the same room 
         return JsonResponse({'name':player.user.username, 'title':nextRoom.title, 'description':nextRoom.description, 'players':players, 'error_msg':""}, safe=True)
     else:
         players = room.playerNames(player_id)
+        # returns player's username, the name of the room they are currently in, its description, the players that are currently in the same room
+        # and an error message that let's you know moving in the direction entered in not an option
         return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
 
@@ -64,4 +72,15 @@ def move(request):
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    message = data['message']
+    room = player.room()
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDs:
+        # broadcasts the player's username, 'says', and the message the player woiuld like to broadcast in the room
+        pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says {message}.'})
+    # returns the player's username, 'says', and the message the player woiuld like to broadcast in the room
+    return JsonResponse({'message':f'{player.user.username}, says {message}'}, safe=True, status=500)
