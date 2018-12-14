4fb8ac4048d8864171f4497a2d4ce6c17653dd75
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-12-13 16:22:29 -0800

    Say function added

--- adventure/api.py
+++ adventure/api.py
@@ -59,9 +59,18 @@ def move(request):
         players = room.playerNames(player_id)
         return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'error_msg':"You cannot move that way."}, safe=True)
 
-
 @csrf_exempt
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    message = data['message']
+    room = player.room()
+    currentPlayerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in currentPlayerUUIDs:
+            pusher.trigger(f'p-channel-{p_uuid}', u'broadcast', {'message':f'{player.user.username} says {message}.'})
+    return JsonResponse({'username':player.user.username, 'message': message}, safe=True, status=200)
+
