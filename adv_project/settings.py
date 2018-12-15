dc737aa6f0e4a61086cfa2036984401b69827ab1
lyang9 <lyang9@buffalo.edu>
2018-12-14 20:19:03 -0500

    Commented out databases

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
     }
 }
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
