5c7e599ced76097a22b2880b3a37b42fb63c7d7f
mrsteveson <patrick.steveson727@gmail.com>
2019-07-29 18:07:51 -0500

    working on say endpoint

--- adventure/api.py
+++ adventure/api.py
@@ -64,4 +64,18 @@ def move(request):
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
-    return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    # return JsonResponse({'error':"Not yet implemented"}, safe=True, status=500)
+    player = request.user.player
+    player_id = player.id
+    player_uuid = player.uuid
+    data = json.loads(request.body)
+    message = data['message']
+    room = player.room()
+    playerUUIDs = room.playerUUIDs(player_id)
+    for p_uuid in playerUUIDs:
+        pusher.trigger(f'p-channel-p{p_uuid}', u'broadcast', {'message':f'{player.user.username}: {message}'})
+    
+    players = room.playerNames(player_uuid)
+    return JsonResponse({'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players, 'message':message}, safe=True)
+
+    
