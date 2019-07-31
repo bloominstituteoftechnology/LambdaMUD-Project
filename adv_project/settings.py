2dcbd46fb8535865da6ad7ed0fd2bedea26beb4a
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-31 12:16:47 -0500

    added conditional for production / development value for ALLOWED_HOST

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = ['.maze-mud-server.herokuapp.com','.maze-mud-client.herokuapp.com']
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
