1fe8a339ccee4a738f3c2f450fd2c7abdfa6a780
adrian <adrianadames@gmail.com>
2020-01-27 22:49:05 -0500

    Changed some settings in order to deploy heroku backend.

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -95,17 +95,17 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 # # FOR RUNNING DATABASE ON LOCAL SERVER
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# # FOR RUNNING DATABASE ON HEROKU SERVER
 # DATABASES = {
-#     'default': dj_database_url.config(default = config('DATABASE_URL')),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+# FOR RUNNING DATABASE ON HEROKU SERVER
+DATABASES = {
+    'default': dj_database_url.config(default = config('DATABASE_URL')),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -164,5 +164,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import django_heroku
 django_heroku.settings(locals())
 
-# # FOR RUNNING DATABASE ON LOCAL SERVER
-del DATABASES['default']['OPTIONS']['sslmode']
+# # # FOR RUNNING DATABASE ON LOCAL SERVER
+# del DATABASES['default']['OPTIONS']['sslmode']
